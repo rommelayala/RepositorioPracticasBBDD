db9e8fc29ff80a64b4c08505d28bd627689720ae
Rommel <rommelsuscripciones@gmail.com>
2014-03-12 13:09:32 +0100

    Practica 3 ejercico 2 A y B

--- BBDD_Practica_3/src/conexion/ConsultaStatement.java
+++ BBDD_Practica_3/src/conexion/ConsultaStatement.java
@@ -1,59 +1,73 @@
 package conexion;
+
 import java.sql.*;
 
 /**
- * Ejemplo de uso de JDBC:
- * -Se establece la cadena de conexión con la base de datos Libros
- * -Se obtienen todos los libros del autor cuyo codigo=5
- * -Se muestran los datos por pantalla 
+ * Ejemplo de uso de JDBC: -Se establece la cadena de conexión con la base de
+ * datos Libros -Se obtienen todos los libros del autor cuyo codigo=5 -Se
+ * muestran los datos por pantalla
  */
 public class ConsultaStatement {
 
-   public static void main(String[] args) {
-      try {
-         Conexion.setURL("jdbc:mysql://localhost:3306/Libros?user=usuario1&password=usuario1");
-         Statement sentenciaSQL = Conexion.getConexion().createStatement();
-          
-         StringBuffer sb = new StringBuffer();
-         
-         sb.append("SELECT * from libros ");
-         sb.append("WHERE titulo LIKE '%java%';");
-      
-         System.out.println(sb.toString());
-         
-         ResultSet res =sentenciaSQL.executeQuery(sb.toString());
-         
-         
-         String codigolibro;
-         String isbn;
-         String titulo;
-         String editorial;
-         String fecha;
-         res.beforeFirst();
-         
-         while (res.next()) {
-         	System.out.println("\nCODIGO\tISBN\t\tTITULO");
-         	
-         	codigolibro = res.getString("codigolibro").toString();
-         	isbn 		= res.getString("isbn").toString();
-         	titulo 		= res.getString("titulo").toString();
-         	System.out.println(codigolibro+"\t"+isbn+"\t"+titulo);
-         	
-         	System.out.println("EDITORIAL\tFECHA");
-         	
-         	editorial = res.getString("editorial").toString();
-         	fecha = res.getString("fecha").toString();
-         	System.out.println(editorial+"\t"+fecha);
-         	
-         	System.out.println("-----------------------");
-         }
-         
-			Conexion.desconecta();
-         
-      } catch (SQLException e) {
-			System.out.println("SQLException: " + e.getMessage());
-			System.out.println("SQLState: " + e.getSQLState());
-			System.out.println("VendorError: " + e.getErrorCode());
-      }
-   }
+	static {
+		Conexion.setURL("jdbc:mysql://localhost:3306/Libros?user=usuario1&password=usuario1");
+	}
+
+	public static void main(String[] args) {
+
+		String consulta = "SELECT * from libros WHERE titulo LIKE '%java%';";
+		
+		ResultSet res = lanzarSelect(consulta);
+		
+		mostrarSelect(res);
+
+		Conexion.desconecta();
+	}
+
+	private static ResultSet lanzarSelect(String select) {
+
+		ResultSet res0=null;
+		
+		try {
+			Statement sentenciaSQL = Conexion.getConexion().createStatement();
+			res0 = sentenciaSQL.executeQuery(select);
+			res0.beforeFirst();
+			return res0;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return res0;
+	}
+	
+	private static void mostrarSelect(ResultSet res){
+		
+		String codigolibro;
+		String isbn;
+		String titulo;
+		String editorial;
+		String fecha;
+		
+		try {
+			while (res.next()) {
+			System.out.println("\nCODIGO\tISBN\t\tTITULO");
+
+			codigolibro = res.getString("codigolibro").toString();
+			isbn = res.getString("isbn").toString();
+			titulo = res.getString("titulo").toString();
+			System.out.println(codigolibro + "\t" + isbn + "\t" + titulo);
+
+			System.out.println("EDITORIAL\tFECHA");
+
+			editorial = res.getString("editorial").toString();
+			fecha = res.getString("fecha").toString();
+			System.out.println(editorial + "\t" + fecha);
+
+			System.out.println("-----------------------");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
