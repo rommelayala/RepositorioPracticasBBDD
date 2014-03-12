eacacd6b97d22a5196a018c61e32655e1bd6073f
Rommel <rommelsuscripciones@gmail.com>
2014-03-12 13:11:04 +0100

    practica 3 ejercicio 2 partes A y B

---
+++ BBDD_Practica_3/test/testConexion/testConexion.java
@@ -0,0 +1,18 @@
+package testConexion;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import conexion.Conexion;
+
+public class testConexion {
+
+	@Test
+	public void test() {
+				
+		
+		//assertTrue("conexion ok", conn);
+	}
+
+}
