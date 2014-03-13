87e351cfb229ed9a2f5bb3f6de802c4cfaa2a904
Rommel <rommelsuscripciones@gmail.com>
2014-03-13 14:33:43 +0100

    ultima de hoy

--- BBDD_Practica_3/src/conexion/ConsultaStatement.java
+++ BBDD_Practica_3/src/conexion/ConsultaStatement.java
@@ -12,27 +12,39 @@ public class ConsultaStatement {
 	static ResultSet res=null;//atributo para guardar el resulset
 	static ResultSetMetaData infoTabla=null;//atributo para guardar la informacino de la tabla
 	
-	static {
-		Conexion.setURL("jdbc:mysql://localhost:3306/Libros?user=usuario1&password=usuario1");
-	}
-	
+	static {Conexion.setURL("jdbc:mysql://localhost:3306/Libros?user=usuario1&password=usuario1");}
+	static Statement sentenciaSQL;
 
 	public static void main(String[] args) {
 
-		String consulta = "SELECT * from libros WHERE titulo LIKE '%java%';";
+		String consulta = "SELECT * from libros;";
 		
-		/* 1 Lanza consulta, devuele un resulset, obtiene y guarda nformacion del resultset*/
+		/** 1 Lanza consulta, devuele un resulset, obtiene y guarda nformacion del resultset*/
 		res = lanzarSelect(consulta);
 		
-		/*2 muestra el contenido del resulset obtenido */
+		/**2 muestra el contenido del resulset obtenido */
 		mostrarSelect(res);
 		
-		/*3 obtiene nuevo Id de la tabla libros*/
+		/**3 obtiene nuevo Id de la tabla libros*/
 		int nuevoId=getNuevoID("libros", "codigoLibro");
 		
-		System.out.println(nuevoId);
+		System.out.println("El nuevo ID es "+nuevoId);
 		
-		/*cierra conexion con la db*/
+		/**4 permite insertar o actualizar la tabla por medio de un String orden
+		 * filasAfectadas.- muestra el numero de filas que ha insertado o actualizado*/
+		String actualizaGrabaFila = "INSERT INTO libros VALUES ("+nuevoId+",0-777-88899-9,'La vida de PI','Melonia','1900-04-12')";//||"UPDATE Libros SET Fecha='2002-01-31' WHERE CodigoLibro=1;"
+		final int filasAfectadas;
+		
+		
+		filasAfectadas=grabarFila(actualizaGrabaFila);
+		System.out.println(filasAfectadas+" Fila(s) afectadas");
+		
+		/**5 añade nuevo autor a la tabla autores */
+		String consulta2 = "SELECT * from autores;";
+		res = lanzarSelect(consulta);
+		
+		
+		/**cierra conexion con la db*/
 		Conexion.desconecta();
 	}
 
@@ -42,7 +54,7 @@ public class ConsultaStatement {
 		ResultSet res0=null;
 		
 		try {
-			Statement sentenciaSQL = Conexion.getConexion().createStatement();
+			sentenciaSQL = Conexion.getConexion().createStatement();
 			res0 = sentenciaSQL.executeQuery(select);
 			res0.beforeFirst();
 			return res0;
@@ -91,18 +103,32 @@ public class ConsultaStatement {
 			e.printStackTrace();
 		}
 	}
+	/*3 obtiene nuevo Id de la tabla libros*/
 	private static int getNuevoID(String tabla, String ID){
 		Integer id=null;
+		//infoTabla.getCatalogName(column)
 		try {
 			res.last();
 			id=res.getInt(ID);
 			
-			System.out.println("\nEl Elemento es ->"+id);
+			System.out.println("\nEl ultimo elemento es ->"+id);
 			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return id+1;
+	}
+	/*4 permite insertar o actualizar la tabla por medio de un String orden*/
+	private static int grabarFila(String orden){
+		int filas=-1;
+		
+		try {
+			filas=sentenciaSQL.executeUpdate(orden);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return filas;
 	}
 }
